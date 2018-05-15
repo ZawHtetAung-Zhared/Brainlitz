261b0fa51a65ddc09b36189044fa20f7aca48a7c
eimon <eimon1150@gmail.com>
2018-05-15 12:04:21 +0630

    create base folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule }    from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LocationComponent } from './components/location/location.component';
+import { UsersComponent } from './components/users/users.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { AppRoutingModule } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    LocationComponent,
+    UsersComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { Component } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LocationComponent } from './components/location/location.component';
+import { UsersComponent } from './components/users/users.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+
+
+export const routes: Routes = [
+    
+    {
+        path: '',
+        component: DashboardComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+      path:'**',
+      component:  PagenotfoundComponent
+    }
+];
+
+export const AppRoutingModule: ModuleWithProviders = RouterModule.forRoot(routes,{ useHash: true });
+
