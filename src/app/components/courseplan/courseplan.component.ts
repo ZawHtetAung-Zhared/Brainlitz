18c4c496a0990cf70078ab2e3492181c9dab89fb
Htet Htet Aung <htethtetaung@Htets-iMac.local>
2018-05-23 09:12:57 +0630

    add create plan modal

--- src/app/components/courseplan/courseplan.component.ts
+++ src/app/components/courseplan/courseplan.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-courseplan',
@@ -7,9 +9,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseplanComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+
   }
+  	courseCategories: any[] = [
+	    {	
+	     	"id": "1",	
+	     	"name": "Category1"
+	    },
+	    {
+	      	"id": "2",	
+	     	"name": "Category2"
+	    },
+	    {
+	      	"id": "3",	
+	     	"name": "Category3"
+	    },
+	    {
+	      	"id": "4",	
+	     	"name": "Category4"
+	    }
+  	];
+
+  	public showModal: boolean = false;
+  	public showsubModal: boolean = true;
+  	public checked: boolean = false;
+  	public modalReference: any;
+  	public closeResult: any;
+
+  	open(content){
+  		// this.modalService.open(content, { size: 'lg' });
+  		this.showModal = true;
+  		this.showsubModal = false;
+  		this.checked = false;
+
+  		this.modalReference = this.modalService.open(content, { size: 'lg' });
+		  this.modalReference.result.then((result) => {
+		  this.closeResult = `Closed with: ${result}`;
+		}, (reason) => {
+		  this.closeResult = `Closed with: ${reason}`;
+		});
+  	}
+
+  	selectedRadioId(data, opensubModal){
+  		this.showModal = false;
+  		this.showsubModal = true;
+  	}
+
+  	back(){
+  		this.showModal = true;
+  		this.showsubModal = false;
+  	}
+
+  	checkedData(data, e){
+  		if(e.target.checked == true){
+  			this.checked = true;
+  		}
+  		else {
+  			this.checked = false;
+  		}
+  		console.log(data)
+  	}
+
+  	createdPlan() {
+  		console.log('created')
+  		this.modalReference.close();
+  	}
 
 }
