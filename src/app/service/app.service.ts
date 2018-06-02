85ad7df0d5f047035c595339480dfc2f033eca99
Htet Htet Aung <htethtetaung@Htets-iMac.local>
2018-06-02 14:51:07 +0630

    holidays add

--- src/app/service/app.service.ts
+++ src/app/service/app.service.ts
@@ -6,7 +6,7 @@ import { Http, Response, RequestOptions, Headers,URLSearchParams } from '@angula
 import { environment } from '../../environments/environment';
 
 // const httpOptions = {
-//     headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA'})
+//     headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM'})
 // };
  
 @Injectable()
@@ -63,7 +63,7 @@ export class appService{
     getLocations(id: string): Observable<any>{
     	let url = this.baseUrl + '/' + id + '/locations';
   		const httpOptions = {
-  	        headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+  	        headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
   	    };
          	return this.httpClient.get(url, httpOptions)
         	.map((res:Response) => {
@@ -77,7 +77,7 @@ export class appService{
       console.log(id)
       let url = this.baseUrl+ '/' + id + '/user';
       const httpOptions = {
-        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.get(url, httpOptions)
       .map((res:Response) => {
@@ -93,7 +93,7 @@ export class appService{
     	let apiUrl = this.baseUrl + '/' + id + '/locations';
 
     	const httpOptions = {
-	        headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+	        headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
 	    };
 
 	    console.log(httpOptions)
@@ -110,7 +110,7 @@ export class appService{
     getSingleLocation(id:string){
       let apiUrl = this.baseUrl  + '/locations/' + id;
       const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.get(apiUrl, httpOptions)
       .map((res:Response) => {
@@ -125,7 +125,7 @@ export class appService{
       console.log(body)
       let apiUrl = this.baseUrl  + '/locations/' + id;
       const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.put(apiUrl,body, httpOptions)
       .map((res:Response) => {
@@ -139,7 +139,7 @@ export class appService{
       console.log(id)
       let apiUrl = this.baseUrl  + '/locations/' + id;
       const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.delete(apiUrl, httpOptions)
       .map((res:Response) => {
@@ -154,7 +154,7 @@ export class appService{
     	let apiUrl = this.baseUrl + '/signup';
     	// let body = JSON.stringify(data);
     	const opt = {
-	        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+	        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
 	    };
 	    return this.httpClient.post(apiUrl, data, opt)
       	.map((res:Response) => {
@@ -168,7 +168,7 @@ export class appService{
       console.log(id)
       let url = this.baseUrl + '/' + id + '/category';
       const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
         return this.httpClient.get(url, httpOptions)
         .map((res:Response) => {
@@ -181,7 +181,7 @@ export class appService{
     createCourse(id: string, data: object): Observable<any>{
       let url = this.baseUrl + '/' + id + '/course';
       const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.post(url, data, httpOptions)
       .map((res:Response) => {
@@ -194,7 +194,7 @@ export class appService{
     createCoursePlan(id: string, data: object): Observable<any>{
       let url = this.baseUrl + '/' + id + '/courseplan';
       const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.post(url, data, httpOptions)
       .map((res:Response) => {
@@ -208,7 +208,7 @@ export class appService{
       console.log(id)
       let apiUrl = this.baseUrl  + '/courseplan/' + id;
       const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'Content-Type': 'application/json', 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.delete(apiUrl, httpOptions)
       .map((res:Response) => {
@@ -222,7 +222,7 @@ export class appService{
       console.log(data);
       let apiUrl = this.baseUrl + '/' + id + '/category';
       const opt = {
-          headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+          headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
 
       return this.httpClient.post(apiUrl, data, opt)
@@ -237,7 +237,7 @@ export class appService{
       console.log(id)
       let url = this.baseUrl+ '/' + id + '/courseplan';
       const httpOptions = {
-        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.get(url, httpOptions)
       .map((res:Response) => {
@@ -250,7 +250,61 @@ export class appService{
     getAllCourse(id: string): Observable<any>{
       let url = this.baseUrl+ '/' + id + '/course';
       const httpOptions = {
-        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.MmpNT0JsRXE0c0lDN3VqRWRqMEZ0Z01YcFhsWWpGdEc.82xgonmzzknNruGVax04d3khcJt06h4VCZIY8PAzgzA' })
+        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
+      };
+      return this.httpClient.get(url, httpOptions)
+      .map((res:Response) => {
+        let result = res;
+        console.log(result);        
+        return result;
+      }) 
+    }
+
+    createHolidays(id: string, data: object): Observable<any>{
+      let url = this.baseUrl+ '/' + id + '/holidays';
+      const opt = {
+          headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
+      };
+
+      return this.httpClient.post(url, data, opt)
+      .map((res:Response) => {
+        let result = res; 
+        console.log(result)
+        return result;
+      }) 
+    }
+
+    getAllHolidays(id: string): Observable<any>{
+      let url = this.baseUrl+ '/' + id + '/holidays';
+      const httpOptions = {
+        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
+      };
+      return this.httpClient.get(url, httpOptions)
+      .map((res:Response) => {
+        let result = res;
+        console.log(result);        
+        return result;
+      }) 
+    }
+
+    createHolidaysCalendar(id: string, data: object): Observable<any>{
+      let url = this.baseUrl+ '/' + id + '/holidaysCalendar';
+      const opt = {
+          headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
+      };
+
+      return this.httpClient.post(url, data, opt)
+      .map((res:Response) => {
+        let result = res; 
+        console.log(result)
+        return result;
+      }) 
+    }
+
+    getAllHolidaysCalendar(id: string): Observable<any>{
+      let url = this.baseUrl+ '/' + id + '/holidaysCalendar';
+      const httpOptions = {
+        headers: new HttpHeaders({ 'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.SjZkTjZhM1RaNUd3U002YWZraWt0N0tLSVhyVFJOeGY._kdDj2IhnypBe1JvpI8muHIuC4G-dAWvVaHRYfFn6FM' })
       };
       return this.httpClient.get(url, httpOptions)
       .map((res:Response) => {
