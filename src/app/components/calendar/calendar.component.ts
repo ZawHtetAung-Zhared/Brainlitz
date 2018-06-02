d73a396e3d1d15ab805ca15941259a462604ec56
Htet Htet Aung <htethtetaung@Htets-iMac.local>
2018-06-02 18:18:51 +0630

    add calendar holiday

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -14,10 +14,15 @@ export class CalendarComponent implements OnInit {
   constructor(private modalService: NgbModal, private _service: appService) { }
 
   ngOnInit() {
+  	this.getAllHolidays();
   }
 
   	modalReference: any;
 	closeResult: any;
+	regionID: any;
+	chosenHoliday: any;
+	arrayHoliday: Array<any> = [];
+	holidayLists: any;
 
   	open(content){
 		this.modalReference = this.modalService.open(content, { backdrop:'static', windowClass:'animation-wrap'});
@@ -27,5 +32,45 @@ export class CalendarComponent implements OnInit {
 	  	  this.closeResult = `Closed with: ${reason}`;
 	  	});
 	}
+
+	getAllHolidays(){
+		this.regionID = localStorage.getItem('regionId');
+	    this._service.getAllHolidays(this.regionID)
+	    .subscribe((res:any) => {
+	      this.holidayLists = res;
+	      console.log(this.holidayLists)
+	      }, err => {
+	        console.log(err)
+	      })
+  	}
+
+  	ChangeValue(e){
+  		if(e.target.checked == true){
+  			this.arrayHoliday.push(e.target.value);	
+  			console.log(this.arrayHoliday)
+  		}
+  		else {
+  			var index = this.arrayHoliday.indexOf(e.target.value);
+  			this.arrayHoliday.splice(index, 1);
+  			console.log(this.arrayHoliday)
+  		}
+  	}
+
+  	createCalendar(formData){
+  		this.regionID = localStorage.getItem('regionId');
+  		let dataObj = {
+  			"name": formData.name,
+  			"holidays": this.arrayHoliday
+  		}
+  		console.log(dataObj);
+ 		this._service.createHolidaysCalendar(this.regionID,dataObj)
+	    .subscribe((res:any) => {
+	      console.log('success holidayCalendar post',res)
+	      }, err => {
+	        console.log(err)
+	      })
+  		this.arrayHoliday = [];
+  		this.modalReference.close();
+  	}
 
 }
