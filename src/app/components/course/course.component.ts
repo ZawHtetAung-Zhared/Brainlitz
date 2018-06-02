d88b3d61d514b36c797282806c524257f14ed0dd
Htet Htet Aung <htethtetaung@Htets-iMac.local>
2018-06-02 15:08:37 +0630

    add function for holiday before regionid pull

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -8,13 +8,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private _service: appService) { }
 
   ngOnInit() {
   	this.getCourseLists();
   }
+
+  courseList: any;
   getCourseLists(){
-    this._service.getAllCourse(this.regionID)
+    this._service.getAllCourse('id')
     .subscribe((res:any) => {
       console.log(res);
       this.courseList = res;
