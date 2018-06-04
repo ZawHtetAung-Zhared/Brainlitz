58d234d298863f3b1a49062198b2e32664f5c74c
eimon <eimon1150@gmail.com>
2018-06-04 12:18:54 +0630

    production mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiurl: 'https://staging-brainlitz.pagewerkz.com/api/v1',
-  enableDebug: false
+  enableDebug: false,
+  orgID: '5b063e2636f2e0f83cdbac88',
 };
