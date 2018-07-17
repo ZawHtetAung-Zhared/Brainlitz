8360816b8182772516f30649f52036a8e8a18628
eimon <eimon1150@gmail.com>
2018-07-17 10:29:44 +0630

    add https of api url in config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
-  apiurl: 'http://app.brainlitz.com',
+  apiurl: 'https://app.brainlitz.com',
   client_id: 'brainlitz_live',
   clientSecret: 'Aylv7M7vG242d3B',
   redirect_uri: 'https://brainlitz-web.pagewerkz.com/#/',
