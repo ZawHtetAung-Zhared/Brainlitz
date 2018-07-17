a4610afd9ce7b1212e56897f8d3fbecbcbb5afa6
Ei Mon <eimon@ip-10-0-3-9.ap-southeast-1.compute.internal>
2018-07-17 05:12:53 +0000

    env: update client id and secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
   apiurl: 'https://app.brainlitz.com',
-  client_id: 'brainlitz_live',
-  clientSecret: 'Aylv7M7vG242d3B',
+  client_id: 'brainlitzlive',
+  clientSecret: 'brainlitzlive',
   redirect_uri: 'https://brainlitz-web.pagewerkz.com/#/',
 };
