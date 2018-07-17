a404899df2e7ed604c2f28cc3fbac1360e7cbaba
eimon <eimon1150@gmail.com>
2018-07-17 10:13:53 +0630

    change config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
-  apiurl: '',
-  client_id: '',
-  clientSecret: '',
-  redirect_uri: '',
+  apiurl: 'http://app.brainlitz.com',
+  client_id: 'brainlitz_live',
+  clientSecret: 'Aylv7M7vG242d3B',
+  redirect_uri: 'https://brainlitz-web.pagewerkz.com/#/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,9 +9,9 @@ export const environment = {
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',  
-  apiurl: '',
+  apiurl: 'http://app.brainlitz.com',
   //apiurl: 'https://staging-brainlitz.pagewerkz.com',  
-  client_id: '',  
-  clientSecret: '',  
-  redirect_uri: '',
+  client_id: 'brainlitz_live',  
+  clientSecret: 'Aylv7M7vG242d3B',  
+  redirect_uri: 'https://brainlitz-web.pagewerkz.com/#/',
 };
