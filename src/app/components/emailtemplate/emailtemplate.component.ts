7b47c6f894343c5eefb7c0c2da60c4ac31372402
eimon <eimon1150@gmail.com>
2018-10-11 16:58:33 +0630

    email text area box

--- src/app/components/emailtemplate/emailtemplate.component.ts
+++ src/app/components/emailtemplate/emailtemplate.component.ts
@@ -10,6 +10,8 @@ export class EmailtemplateComponent implements OnInit {
 
   public selectedType: any = null;
   public concatValue: any;
+  public leftPos: any = 0;
+  public topPos: any = -110;
   public isedit: boolean = true;
   public textareaTxt: string = '';
 	public showBox: boolean = false;
@@ -39,21 +41,36 @@ export class EmailtemplateComponent implements OnInit {
   }
 
   insertVariable(val){
-    this.concatValue = val;
-    $('.input-msg').val( this.textareaTxt +' '+ this.concatValue);
+    this.concatValue = "[[" + val + "]]";
+    console.log(this.textareaTxt)
+    $('.input-msg').val( this.textareaTxt + this.concatValue);
     $('.input-msg').focus();
+    console.log(this.textareaTxt + this.concatValue)
+    var all_txt = this.textareaTxt + this.concatValue;
+    
+    this.textareaTxt = all_txt;
+    this.showBox = false;
   }
 
   changeMethod(word){
     console.log(word)
     this.textareaTxt = word;
     if (word.includes('[[')) {
-      console.log('hi')
-      var str_sub = word.substr(word.lastIndexOf(" ")+1)
-      console.log(str_sub)
-      this.showBox = (str_sub.length == 2) ? true : false;
+      var str_sub = word.substr(word.lastIndexOf(" ")+1)      
+      var str_res = str_sub.split("[[");
+      var index = str_res.length - 1;
+      console.log(word.length)
+      this.leftPos = (word.length * 8) - 112;
+
+      var text = $('.input-msg').val();   
+      var lines = text.split(/\r|\r\n|\n/);
+      var count = lines.length;
+      console.log(count);
+      // this.topPos += 45
+
+      this.showBox = (str_res[index] == '' && str_res != "") ? true : false;
     }else{
-      // this.showBox = false;
+      this.showBox = false;
     }
   } 
 
