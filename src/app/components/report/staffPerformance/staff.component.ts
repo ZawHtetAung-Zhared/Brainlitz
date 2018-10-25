b7de8df51d5c396da5bfa5d8aa67e6c2cd63313e
ahtisham <ahtisham.official@gmail.com>
2018-10-26 00:32:34 +0530

    Adding echarts as dependency,updating typescript config,draw graph

--- src/app/components/report/staffPerformance/staff.component.ts
+++ src/app/components/report/staffPerformance/staff.component.ts
@@ -56,6 +56,7 @@ export class StaffPerformanceReport implements OnInit {
     //   }, err => {
     //     console.log(err)
     //   })
+
     if (staffData) { //check if we have data to show report
       if (this.selectedFilter) {
 
@@ -65,6 +66,74 @@ export class StaffPerformanceReport implements OnInit {
         this.reportData = this.getFinalData(data);
         console.log("result after final step");
         console.log(this.reportData);
+        setTimeout(() => {
+
+
+        if (this.reportData.length) {
+          let echarts = require('echarts');
+          let barColor=[0,'#ffb6b3','#ffd2b3','#ffea80','#c3e6a1','#aaf2c8'];
+          let option = {
+
+            tooltip: {},
+            grid: {
+              top: 30,
+              bottom:10,
+              left:30
+            },
+
+            yAxis: {
+              data: ["1", "2", "3", "4", "5"],
+              axisTick: {show: false},
+              axisLine: {show: false,lineStyle:{
+                color:"#64707d"
+              }},
+              axisLabel: {margin:8},
+
+            },
+            xAxis: {
+              position: 'top',
+              axisTick: {show: false},
+              axisLine: {show: false,lineStyle:{
+                color:"#64707d"
+              }},
+              splitLine: {lineStyle:{color:'#edeff0'}},
+            },
+            barWidth:16,
+            bargap:'10%',
+            series: [{
+              name: 'Rating',
+              type: 'bar',
+              data: []
+            }]
+          };
+          this.reportData.forEach(function (report, index) {
+            console.log(report, index);
+            option.series[0].data=[];
+             let elem = document.getElementById(report.id);
+            console.log(elem);
+             let graph = echarts.init(elem);
+            for (var key in report.rating) {
+              let obj = {
+                value:report.rating[key],
+                name:'Hola',
+                itemStyle: {
+                  color: barColor[key],
+
+                }
+              };
+              option.series[0].data.push(obj);
+              graph.setOption(option);
+            }
+            // report.rating.forEach(function(value,index){
+            //   console.log(value);
+            // });
+
+            option.series[0].data.push(report.rating);
+
+          });
+        }
+         },2000);
+        //
 
       }
     } else {
@@ -85,9 +154,10 @@ export class StaffPerformanceReport implements OnInit {
           "2": 0,
           "1": 0
         },
-        totalRating:0,
-        ratingWeightage:0,
-        averageRating:0
+        totalRating: 0,
+        ratingWeightage: 0,
+        averageRating: 0,
+        id: "graph" + i
       };
       obj.location = data[i].locationName;
       let categories = data[i].categories || [];
@@ -113,9 +183,9 @@ export class StaffPerformanceReport implements OnInit {
         return sum + obj.rating[key];
       }, 0);
       obj.ratingWeightage = Object.keys(obj.rating).reduce(function (sum, key) {
-        return sum+ obj.rating[key]*parseInt(key);
+        return sum + obj.rating[key] * parseInt(key);
       }, 0);
-      obj.averageRating = (obj.ratingWeightage/obj.totalRating).toFixed(2);
+      obj.averageRating = (obj.ratingWeightage / obj.totalRating).toFixed(2);
       result.push(obj);
     }
     return result;
