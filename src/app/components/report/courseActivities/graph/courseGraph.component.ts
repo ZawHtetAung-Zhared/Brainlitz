3307f167fe8699954581c9d4ca07c7faf7683e23
ahtisham <ahtisham.official@gmail.com>
2018-11-01 19:57:46 +0530

    [Course Activities: draw graph for groupBy Location]

--- src/app/components/report/courseActivities/graph/courseGraph.component.ts
+++ src/app/components/report/courseActivities/graph/courseGraph.component.ts
@@ -14,13 +14,130 @@ export class CourseActivitiesReportGraph implements OnInit {
   ngOnInit(){
     console.log("ngonInit");
     console.log(this.reportItems);
+    let _self = this;
+    this.echarts = require('echarts');
+    this.plotOption ={
+      tooltip: {},
+      grid: {
+        top: 60,
+        bottom: 10
+      },
+      textStyle:{
+        fontFamily:'Montserrat-Medium',
+        fontSize:12,
+        color:'#64707d'
+      },
+      yAxis: {
+        data: [],
+        type : 'category',
+        color:"#64707d",
+        axisTick: { show: false },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "#edeff0"
+          }
+        },
+        axisLabel: {
+          show:true,
+          formatter: function (value) {
+            var val = value.split(',');
+            return '{fregment1|'+val[0]+'}' + '\n' + '{fregment2|'+val[1]+' lessons}';
+          },
+          textStyle: {
+            fontSize: 12,
+            lineHeight:16,
+            color:'#64707d'
+          },
+          rich:{
+            fregment1:{
+              fontSize:16,
+              lineHeight:20,
+              color:'#2e3d4d',
+              fontWeight:600,
+            }
+          }
+        },
+        splitLine:{show:false}
+
+      },
+      xAxis: {
+        position: 'top',
+        axisTick: { show: true },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "#edeff0"
+          }
+        },
+        splitLine: { show:false,lineStyle: { color: '#64707d' } },
+      },
+      barWidth: 56,
+      legend: {
+        show:false,
+        itemWidth:16,
+        itemHeight:16,
+        itemGap:30,
+        data: [{name:'Present',textStyle:{}}, 'Absent', 'Not taken'],
+        formatter: function (value) {
+          return value ;
+        },
+        rich:{
+          fregment2:{
+            borderRadius:16
+          }
+        }
+      },
+      series: [{
+        name: 'Present',
+        type: 'bar',
+        stack: 'courseActivities',
+        itemStyle: {
+          normal: { color: '#91f2ba'},
+        },
+        symbolSize:5,
+        data: []
+      }, {
+        name: 'Absent',
+        type: 'bar',
+        stack: 'courseActivities',
+        itemStyle: {
+          normal: { color:'#ffaaa6' }
+        },
+        data: []
+      }, {
+        name: 'Not taken',
+        type: 'bar',
+        stack: 'courseActivities',
+        itemStyle: {
+          normal: {color: '#edeff0',shadowColor:'red' }
+        },
+        data: []
+
+      }]
+    };
+    let presentData = [];
+    let absentData = [];
+    let notTakenData = [];
+    this.reportItems.forEach(function(item){
+      _self.plotOption.yAxis.data.push(item.groupTypeValue+','+item.lessons.count);
+      presentData.push(item.lessons.present);
+      absentData.push(item.lessons.absent);
+      notTakenData.push(item.lessons.notTaken);
+    });
+    this.plotOption.series[0].data = presentData;
+    this.plotOption.series[1].data = absentData;
+    this.plotOption.series[2].data = notTakenData;
 
   }
 
   ngAfterViewInit(){
-    // var elem = document.getElementById();
-    // let graph = this.echarts.init(elem);
-    // graph.setOption(this.plotOption);
+     var elem = document.getElementById('courseActiviesGraph');
+    console.log(elem);
+    console.log(this.plotOption);
+    elem.style.height = (this.reportItems.length * 100) +'px';
+    let graph = this.echarts.init(elem);
+    graph.setOption(this.plotOption);
   }
 
 }
