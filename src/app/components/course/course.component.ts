358c950190aeba86e2ba7e14e431b684c35cf98d
Htet Shwe Yi Lin <htetshweyilin@Shweyi.local>
2018-11-09 09:43:36 +0630

    add new modal to course component

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -67,7 +67,7 @@ export class CourseComponent implements OnInit {
   showList:boolean = false;
   
   public draft:boolean;
-  public selectedCustomer:any;
+  public selectedCustomer:any = {};
 
   constructor( @Inject(DOCUMENT) private doc: Document, private router: Router, private _service: appService, public dataservice: DataService, private modalService: NgbModal, public toastr: ToastsManager, public vcr: ViewContainerRef ) {
     this.toastr.setRootViewContainerRef(vcr);
@@ -524,10 +524,11 @@ export class CourseComponent implements OnInit {
     });
   }
 
-  cancelModal(){
+  cancelModal(type){
     this.modalReference.close();
     this.isSeatAvailable = true;
     this.showList = false;
+    this.selectedCustomer = {};
   }
 
   getAllUsers(type){
@@ -583,6 +584,7 @@ export class CourseComponent implements OnInit {
   }
 
   getSingleCustomer(ID, state){
+    console.log("this.selectedCustomer",this.selectedCustomer)
     this._service.editProfile(this.regionId, ID)
     .subscribe((res:any) => {
       console.log('selected Customer',res);
