ed1ad11284d29fd2a32358cf30e3af94f84a77c3
ahtisham <ahtisham.official@gmail.com>
2018-12-04 15:41:19 +0530

    adding layout for studentEnrolment

--- src/app/components/report/studentEnrollment/enrollment.component.ts
+++ src/app/components/report/studentEnrollment/enrollment.component.ts
@@ -1,15 +1,152 @@
 import {Component, OnInit} from '@angular/core';
-
+import {DaterangepickerConfig} from 'ng2-daterangepicker';
+import {NgbModal, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
+import {appService} from '../../../service/app.service';
 @Component({
   selector: 'student-enrollment-report',
   templateUrl: './enrollment.component.html',
   styleUrls: ['../report.component.css'] // we share same style url for all nested report component
 })
-export class StudentEnrollmentReport implements OnInit{
-  constructor() {
-    window.scroll(0, 0);
-  }
-  ngOnInit() {
+export class StudentEnrollmentReport implements OnInit {
+  stdEnrolGroupByList = ['Location', 'Category', 'Course Plan'];
+  filterList = ['Category', 'Course Plan', 'Course Name', 'Location'];
+  categoryList = ['Art & Science', 'Dance', 'Education', 'Sports', 'Technology'];
+  locationList = ['Woodland', 'Yishun', 'Admiralty', 'Bedok', 'Sembawang'];
+  coursePlanList = ['Advanced', 'Beginner', 'Individual', 'Weekend'];
+  courseNameList = ['Business Administration', 'Management Studies', '3D Animation', 'Facebook Marketing', 'Cyber Security', 'Math Classes', 'Orchestra', 'Guitar', 'Hip Hop', 'Piano', 'Meditation & Yoga', 'Health & Fitness', 'Sports Science'];
+  searchResult:any;
+  groupBy = "location";
+  selectedFilter:any;
+  filter:any;
+  modalReference:any;
+  reportData:any;
+  selectedFilterType:any;
+  daterange:any = {};
+  options:any;
 
+  constructor(private daterangepickerOptions:DaterangepickerConfig, private modalService:NgbModal, private _service:appService) {
+    window.scroll(0, 0);
+    this.daterangepickerOptions.settings = {
+      locale: {format: 'd m YYYY'},
+      alwaysShowCalendars: true,
+      ranges: {
+        'Today': [moment()],
+        'Yesteday': [moment().subtract(1, 'days'), moment()],
+        'Last Month': [moment().subtract(1, 'month'), moment()],
+        'Last 3 Months': [moment().subtract(4, 'month'), moment()],
+        'Last 6 Months': [moment().subtract(6, 'month'), moment()],
+        'Last 12 Months': [moment().subtract(12, 'month'), moment()],
+        'Last 18 Months': [moment().subtract(18, 'month'), moment()],
+      }
+    };
+  }
+
+  ngOnInit() {
+    this.selectedFilter = "";
+    this.selectedFilterType = '0';
+    this.filter = {type: "", 'value': []};
+    this.searchResult = {
+      show: false,
+      value: []
+    };
+    this.options = {
+      startDate: moment().startOf('hour'),
+      endDate: moment().startOf('hour').add(32, 'hour'),
+      locale: {format: 'ddd, DD MMM YYYY'},
+      alwaysShowCalendars: true,
+    };
+  }
+  updateGraphUsingGroupBy(event) {
+    this.filter = {
+      value: []
+    };
+    switch (event.target.value) {
+      case "Location":
+        this.groupBy = "location";
+        //this.showReportByLocation();
+        break;
+      case "Category":
+        this.groupBy = "category";
+        //this.showReportByCategory();
+        break;
+      case "Course Plan":
+        this.groupBy = "coursePlan";
+        //this.showReportByCoursePlan();
+        break;
+      default:
+        this.groupBy = "location";
+        //this.showReportByLocation();
+
+
+    }
+  }
+  showFilterModal(content) {
+    this.searchResult.show = false;
+    this.searchResult.value = this.categoryList;
+    this.filter = {
+      type: "category",
+      value: []
+    };
+    this.modalReference = this.modalService.open(content, {
+      backdrop: 'static',
+      windowClass: 'animation-wrap',
+      size: 'lg'
+    });
+
+    this.modalReference.result.then((result) => {
+      console.log(result);
+    }, (reason) => {
+      console.log(reason);
+    });
+  }
+
+  removeCurrentFilter(value) {
+    this.filter.value = this.filter.value.filter(e => e !== value);
+    this.searchResult.value.push(value);
+    if (!this.filter.value.length) {
+      this.filter.type = ""
+    }
+    ;
+    this.applyFilters();
+  }
+
+  removeAllFilters() {
+    this.filter = {
+      type: "",
+      value: []
+    };
+    this.applyFilters();
+  }
+  clearSearch() {
+
+  }
+  filterSearch(value) {
+    if (value) {
+      this.searchResult.show = true;
+    } else {
+      this.searchResult.show = false;
+    }
+  }
+
+  selectFilter(value) {
+    this.filter.value.push(value);
+    this.searchResult.show = false;
+    this.searchResult.value = this.searchResult.value.filter(e => e !== value);
+  }
+  applyFilters() {
+    switch (this.groupBy) {
+      case "location":
+        //this.showReportByLocation();
+        break;
+      case "category":
+        //this.showReportByCategory();
+        break;
+      case "coursePlan":
+        //this.showReportByCoursePlan();
+        break;
+    }
+
+    this.modalReference.close();
   }
 }
