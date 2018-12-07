6c82fb85b63bc3c7b7b9f85f387d4f9ff23c8b1f
ahtisham <ahtisham.official@gmail.com>
2018-12-07 18:03:48 +0530

    [MonthlyActiveStudents : plot graph]

--- src/app/components/report/monthlyActiveStudents/graph/masGraph.component.ts
+++ src/app/components/report/monthlyActiveStudents/graph/masGraph.component.ts
@@ -6,12 +6,86 @@ import {Component, OnInit,Input} from '@angular/core';
 
 export class MonthlyActiveStdReportGraph implements OnInit {
   @Input() reportItems: any;
+  plotOption:any;
+  echarts:any;
+  barColor:any;
   ngOnInit(){
     console.log("inside graph component");
     console.log(this.reportItems);
+    this.echarts = require('echarts');
+    let _self = this;
+    this.plotOption = {
+      tooltip: {
+        backgroundColor:'#3d454d',
+      },
+      textStyle:{
+        fontFamily:'Montserrat-Medium'
+      },
+      grid: {
+        top: 30,
+        bottom:10,
+        left:150
+      },
+
+      yAxis: {
+        data: [],
+        axisTick: {show: false},
+        axisLine: {show: false,lineStyle:{
+          color:"#64707d"
+        }},
+        axisLabel: {
+          fontSize:16,
+          fontWeight:600,
+          color:'#2e3d4d',
+          margin:8
+        },
+
+      },
+      xAxis: {
+        position: 'top',
+        axisTick: {show: true,lineStyle:{
+          color:'#edeff0'
+        }},
+        axisLabel:{
+          color:'#64707d'
+        },
+        axisLine: {show: true,lineStyle:{
+          color:"#edeff0"
+        }},
+        splitLine: {lineStyle:{color:'#edeff0'}},
+      },
+      barWidth:40,
+      itemStyle:{
+        color:'#80bfff'
+      },
+      bargap:'10%',
+      series: [{
+        name: 'Monthly Active Students',
+        type: 'bar',
+        data: []
+      }]
+    };
+    this.reportItems.forEach(function(item){
+      _self.plotOption.yAxis.data.push(item.groupTypeValue);
+      _self.plotOption.series[0].data.push(item.students);
+    });
+    // for (var key in this.reportItems.rating) {
+    //   let obj = {
+    //     value: this.reportItems.rating[key],
+    //     name: 'Hola',
+    //     itemStyle: {
+    //       color: this.barColor[key],
+    //
+    //     }
+    //   };
+    //   this.plotOption.series[0].data.push(obj);
+    //
+    // }
   }
   ngAfterViewInit(){
-
+    var elem = document.getElementById('masGraph');
+    let graph = this.echarts.init(elem);
+    graph.setOption(this.plotOption);
   }
 
 }
