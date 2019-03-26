7048cb7f69a7e71ffd9acac3cf16eb5a00558825
Wai Yan Aung <waiyanaung@amdon.com>
2019-03-26 17:15:16 +0630

    sticky bar for concept create

--- src/app/components/testwerkz/testwerkz.component.ts
+++ src/app/components/testwerkz/testwerkz.component.ts
@@ -2042,7 +2042,7 @@ export class TestwerkzComponent implements OnInit {
     };
     const tempContentArray = [];
     question.contents.map( (contentObj,index) => {
-      _this.changeTimeFormat(contentObj,'asdasd')
+      _this.changeTimeFormat(contentObj,'temp')
       if(contentObj.duration){
         var tempVideoContentObj = {
           contentId: "",
@@ -2133,7 +2133,7 @@ export class TestwerkzComponent implements OnInit {
     };
     const tempContentArray = [];
     pd.contents.map((contentObj, index) => {
-      _this.changeTimeFormat(contentObj,'asdasd')
+      _this.changeTimeFormat(contentObj,'temp')
       if(contentObj.duration){
         var tempVideoContentObj = {
           contentId: "",
