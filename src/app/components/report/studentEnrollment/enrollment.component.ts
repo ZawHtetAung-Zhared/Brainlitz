2ad4bbba6bd4c4481d3754f9b2fcc2a4859285f1
ahtisham <ahtisham.official@gmail.com>
2019-05-13 09:54:17 +0530

    [StudentEnrolment: Fix: filter suggestion does't show on init]

--- src/app/components/report/studentEnrollment/enrollment.component.ts
+++ src/app/components/report/studentEnrollment/enrollment.component.ts
@@ -30,6 +30,7 @@ export class StudentEnrollmentReport implements OnInit {
   filterModel:any;
   startDate:any;
   endDate:any;
+  initFilter = true;
   public regionID = localStorage.getItem('regionId');
   @BlockUI() blockUI: NgBlockUI;
   constructor(private daterangepickerOptions:DaterangepickerConfig, private modalService:NgbModal, private _service:appService) {
@@ -199,6 +200,10 @@ export class StudentEnrollmentReport implements OnInit {
     _self.locationList = Array.from(new Set(_self.locationList));
     _self.coursePlanList = Array.from(new Set(_self.coursePlanList));
     _self.courseNameList = Array.from(new Set(_self.courseNameList));
+    if(_self.initFilter){
+      _self.searchResult.value = _self.categoryList;
+      _self.initFilter = false;
+    }
     return res;
   }
 
