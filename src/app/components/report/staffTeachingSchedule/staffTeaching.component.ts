9892415ed529205f377616effcd051508e4d098f
ahtisham <ahtisham.official@gmail.com>
2019-05-13 09:39:10 +0530

    [StaffTeaching: Fix: filter suggestion does't show on init]

--- src/app/components/report/staffTeachingSchedule/staffTeaching.component.ts
+++ src/app/components/report/staffTeachingSchedule/staffTeaching.component.ts
@@ -27,6 +27,7 @@ export class StaffTeachingScheduleReport implements OnInit {
   filterModel:any;
   startDate:any;
   endDate:any;
+  initFilter = true;
   public regionID = localStorage.getItem('regionId');
   @BlockUI() blockUI: NgBlockUI;
 
@@ -207,6 +208,10 @@ export class StaffTeachingScheduleReport implements OnInit {
     _self.locationList = Array.from(new Set(_self.locationList));
     _self.coursePlanList = Array.from(new Set(_self.coursePlanList));
     _self.courseNameList = Array.from(new Set(_self.courseNameList));
+    if(_self.initFilter){
+      _self.searchResult.value = _self.categoryList;
+      _self.initFilter = false;
+    }
     return res;
   }
 
