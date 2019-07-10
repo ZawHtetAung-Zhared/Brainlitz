2abbc999fe58978878f18ba8cc745661fb3cc07e
Htet Shwe Yi Lin <htetshweyilin@Shweyi.local>
2019-07-10 10:40:06 +0630

    change environment.ts,environment.prod.ts and gitignore at live

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-/// The file contents for the current environment will overwrite these during build.
+// The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
@@ -9,6 +9,5 @@ export const environment = {
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
-  apiurl: 'http://dev-app.brainlitz.com'
-  // apiurl: 'https://staging-brainlitz.pagewerkz.com'
+  apiurl: 'https://app.brainlitz.com'
 };
