472428b301ba3e04bc3afb629d7de0bea5959b8c
Htet Shwe Yi Lin <htetshweyilin@Shweyi.local>
2019-07-10 10:21:05 +0630

    chang  environment.ts and environment.prod.ts and gitinnore for dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
-  apiurl: 'https://app.brainlitz.com'
+  apiurl: ''
 };
