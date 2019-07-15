04cd10820dd3a51f5418992eedd2c69231278249
Wai Yan Aung <waiyanaung@amdon.com>
2019-07-15 11:34:49 +0630

    clear value for reschedule class list

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -3612,6 +3612,7 @@ export class CourseComponent implements OnInit {
   isReschedule: boolean = false;
   getReschedule(reschedule, user) {
     this.isReschedule = false;
+    this.resechduleList = [];
     this.modalReference = this.modalService.open(reschedule, {
       backdrop: 'static',
       windowClass:
