4fd59c1e988167222d3806e53e0386697f54285a
Wai Yan Aung <waiyanaung@amdon.com>
2019-08-27 13:01:19 +0630

    fix update color plette

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
-  apiurl: 'http://dev-app.brainlitz.com'
-  // apiurl: 'https://staging-brainlitz.pagewerkz.com'
+  // apiurl: 'http://dev-app.brainlitz.com'
+  apiurl: 'https://staging-brainlitz.pagewerkz.com'
 };
