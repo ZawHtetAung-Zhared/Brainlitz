8bc74ef8ae5f9b49e2727debbb5319bcfb4daf2d
Zune Zume <seinmama@amdon.com>
2019-09-11 14:58:09 +0630

    bug fix worng month in shedulemodal

--- src/app/service/pipe/utc-date-and-day.pipe.ts
+++ src/app/service/pipe/utc-date-and-day.pipe.ts
@@ -1,17 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment-timezone';
+import { months, monthsShort } from 'moment';
 
 @Pipe({
   name: 'utcDateAndDay'
 })
 export class UtcDateAndDayPipe implements PipeTransform {
   transform(getDate): any {
-    if (true) {
-      getDate = moment(getDate).format('YYYY-MM-DD');
-      // console.warn(getDate)
-    }
+    // if (true) {
+    //   console.error(monthsShort(getDate.month-1))
+    //   getDate = moment(getDate).format('YYYY-MM-DD');
+    //   console.error(getDate)
+    //   console.error(new Date(getDate).getUTCMonth());
 
-    let d = new Date(getDate).getUTCDay();
+    // }
+
+    let d = new Date(getDate.month);
     const monthNames = [
       'Jan',
       'Feb',
@@ -27,40 +31,35 @@ export class UtcDateAndDayPipe implements PipeTransform {
       'Dec'
     ];
     let fullDay;
-    switch (d) {
-      case 0:
-        fullDay = 'Sun';
-        break;
-      case 1:
-        fullDay = 'Mon';
-        break;
-      case 2:
-        fullDay = 'Tue';
-        break;
-      case 3:
-        fullDay = 'Wed';
-        break;
-      case 4:
-        fullDay = 'Thu';
-        break;
-      case 5:
-        fullDay = 'Fri';
-        break;
-      case 6:
-        fullDay = 'Sat';
-    }
-    var monthName = monthNames[new Date(getDate).getUTCMonth()];
-    var yearName = new Date(getDate).getUTCFullYear();
+    // switch (d) {
+    //   case 0:
+    //     fullDay = 'Sun';
+    //     break;
+    //   case 1:
+    //     fullDay = 'Mon';
+    //     break;
+    //   case 2:
+    //     fullDay = 'Tue';
+    //     break;
+    //   case 3:
+    //     fullDay = 'Wed';
+    //     break;
+    //   case 4:
+    //     fullDay = 'Thu';
+    //     break;
+    //   case 5:
+    //     fullDay = 'Fri';
+    //     break;
+    //   case 6:
+    //     fullDay = 'Sat';
+    // }
+
+    var monthName = monthsShort(getDate.month - 1);
+    var yearName = getDate.year;
     var year = yearName.toString();
     monthName;
     var utcDateAndDay =
-      fullDay +
-      ', ' +
-      new Date(getDate).getUTCDate() +
-      ' ' +
-      monthName +
-      ' ' +
-      year;
+      getDate.dayOfWeek + ', ' + getDate.day + ' ' + monthName + ' ' + year;
     return utcDateAndDay;
   }
 }
