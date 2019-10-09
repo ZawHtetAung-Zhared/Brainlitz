f648550f31bf07d577ee35f144975ff6233193d5
Zune Zune <seinmama@amdon.com>
2019-10-09 14:07:37 +0630

    api integration for make attandance

--- src/app/components/today-lessons/today-lessons.component.ts
+++ src/app/components/today-lessons/today-lessons.component.ts
@@ -40,7 +40,9 @@ export class TodayLessonsComponent implements OnInit {
   expandTodayCourse() {
     this.isExpand = true;
   }
-  onClickRadio(type, id, courseId) {
+  onClickRadio(type, id, index) {
+    console.log('index', index);
+    console.log('course', this.todayCourse.courses[index]._id);
     var d = new Date(this.todayDate).getUTCDate();
     var m = new Date(this.todayDate).getUTCMonth() + 1;
     var y = new Date(this.todayDate).getUTCFullYear();
@@ -55,20 +57,60 @@ export class TodayLessonsComponent implements OnInit {
     console.log(d, '/', m, '/', y);
     console.log('obj~~~', obj);
 
-    this._service.markAttendance(courseId, obj, d, m, y).subscribe(
-      (res: any) => {
-        setTimeout(() => {
-          this.toastr.success(res.message);
-        }, 100);
-        console.log('res', res);
-        // this.getUsersInCourse(this.courseId);
+    this._service
+      .markAttendance(this.todayCourse.courses[index]._id, obj, d, m, y)
+      .subscribe(
+        (res: any) => {
+          setTimeout(() => {
+            this.toastr.success(res.message);
+          }, 100);
+          console.log('res', res);
+          // this.getUsersInCourse(this.courseId);
 
-        // this.getAssignUsers(d, m, y);
-      },
-      err => {
-        console.log(err);
-        this.toastr.error('');
-      }
-    );
+          this.getAssignUsers(d, m, y, index);
+        },
+        err => {
+          console.log(err);
+          this.toastr.error('');
+        }
+      );
+  }
+  getAssignUsers(d, m, y, index) {
+    let prsentCount = 0;
+    let absentCount = 0;
+    //this.blockUI.start('Loading');
+    this._service
+      .getAssignUser(
+        this.regionId,
+        this.todayCourse.courses[index]._id,
+        d,
+        m,
+        y
+      )
+      .subscribe(
+        (res: any) => {
+          console.log(res, 'active course info');
+          let activeCourse = res;
+          for (let j = 0; j < activeCourse.CUSTOMER.length; j++) {
+            this.todayCourse.courses[index].students[j].todayLesson.attendance =
+              activeCourse.CUSTOMER[j].attendance;
+            if (activeCourse.CUSTOMER[j].attendance == true) {
+              prsentCount += 1;
+            } else if (activeCourse.CUSTOMER[j].attendance == false) {
+              absentCount += 1;
+            }
+          }
+          console.log('update course', this.todayCourse.courses[index]);
+          this.todayCourse.courses[index].attendance.present = prsentCount;
+          this.todayCourse.courses[index].attendance.absent = absentCount;
+        },
+        err => {
+          //this.blockUI.stop();
+          console.log(err);
+        }
+      );
+    setTimeout(() => {
+      //this.blockUI.stop();
+    }, 500);
   }
 }
