57ffbcf8a27dedd68d430681f8065718dea57fe7
Wai Yan Aung <waiyanaung@amdon.com>
2019-10-17 08:38:13 +0630

    data binding for remaining lesson

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -2301,7 +2301,6 @@ export class UsersComponent implements OnInit {
   public lessonOfStudent;
   openLessonsModal(modal, course) {
     this.lessonOfStudent = course;
-    console.warn(course);
     this.modalReference = this.modalService.open(modal, {
       backdrop: 'static',
       windowClass: 'modal-xl d-flex justify-content-center align-items-center'
