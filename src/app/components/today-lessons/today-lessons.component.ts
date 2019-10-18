55eca7491b45ece5f044333e5cced11e48a79f37
Zar Zar Kyaw Zin <zarzarkyawzin@gothic.local>
2019-10-18 17:39:50 +0630

    show label for remaining lessos of total lessons

--- src/app/components/today-lessons/today-lessons.component.ts
+++ src/app/components/today-lessons/today-lessons.component.ts
@@ -11,7 +11,6 @@ export class TodayLessonsComponent implements OnInit {
   todayDate: any;
   isExpand: boolean = false;
   todayCourse: any;
-  isEmptyLesson: boolean = true;
   public regionId = localStorage.getItem('regionId');
   public locationID = localStorage.getItem('locationId');
   @Output() courseDetail = new EventEmitter();
@@ -30,10 +29,6 @@ export class TodayLessonsComponent implements OnInit {
 
         this.todayCourse = res;
         console.log('tday lessons', this.todayCourse);
-
-        if (this.todayCourse.courses.length > 0) {
-          this.isEmptyLesson = false;
-        } else this.isEmptyLesson = true;
       },
       err => {
         console.log(err);
