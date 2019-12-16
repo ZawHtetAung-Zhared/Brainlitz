8bb73cce8c97198ec29671b179897d91441046b9
Zar Zar Kyaw Zin <zarzarkyawzin@gothic.local>
2019-12-16 15:49:24 +0630

    fix test specs for unit testing

--- src/app/components/apg/apg.component.spec.ts
+++ src/app/components/apg/apg.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApgComponent } from './apg.component';
+import { DragulaModule, DragulaService } from 'ng2-dragula';
+import { FormsModule } from '@angular/forms';
+import { UserGradingComponent } from './user-grading/user-grading.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { appService } from '../../service/app.service';
+import { HttpModule } from '@angular/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ToastModule } from 'ng5-toastr/ng5-toastr';
 
 describe('ApgComponent', () => {
   let component: ApgComponent;
@@ -8,9 +17,18 @@ describe('ApgComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ApgComponent ]
-    })
-    .compileComponents();
+      declarations: [ApgComponent, UserGradingComponent],
+      imports: [
+        DragulaModule.forRoot(),
+        FormsModule,
+        NgbModule.forRoot(),
+        HttpModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+        ToastModule.forRoot()
+      ],
+      providers: [appService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
