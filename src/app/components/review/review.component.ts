50e6e924529fa21b8fb466a87194024383591387
Amdon User <amdonuser@Tin-Thuzar-Win.local>
2019-12-18 15:25:02 +0630

    added new route for review

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -5,6 +5,7 @@ import {
   EventEmitter,
   Output
 } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-review',
@@ -114,7 +115,7 @@ export class ReviewComponent implements OnInit {
 
   @Output() backto = new EventEmitter();
 
-  constructor() {}
+  constructor(private _location: Location) {}
 
   ngOnInit() {
     if (this.reviewList.length > 0) {
@@ -148,6 +149,9 @@ export class ReviewComponent implements OnInit {
 
     this.activeObj = obj;
     this.activeIndex = index;
+  }
+  backClicked() {
+    this._location.back();
   }
 
   goToPrev() {
