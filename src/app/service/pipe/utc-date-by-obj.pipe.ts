c47e430fd74d804a1f8092f5e13637178871829f
Zune Zune <seinmama@amdon.com>
2020-01-13 09:16:11 +0630

    selected showing date does not show in header of modal when click exiting course in schedule

--- src/app/service/pipe/utc-date-by-obj.pipe.ts
+++ src/app/service/pipe/utc-date-by-obj.pipe.ts
@@ -5,19 +5,17 @@ import { months, monthsShort } from 'moment';
 })
 export class UtcDateByObjPipe implements PipeTransform {
   transform(getDate): any {
+    console.warn(getDate.day.toString().length);
+    console.warn(getDate);
+
     var monthName = monthsShort(getDate.month - 1);
     var yearName = getDate.year;
     var year = yearName.toString();
+    var day =
+      getDate.day.toString().length > 1 ? getDate.day : '0' + getDate.day;
     monthName;
     var utcDateAndDay =
-      getDate.dayOfWeek +
-      ', ' +
-      '0' +
-      getDate.day.slice(-2) +
-      ' ' +
-      monthName +
-      ' ' +
-      year;
+      getDate.dayOfWeek + ', ' + day + ' ' + monthName + ' ' + year;
     return utcDateAndDay;
   }
 }
