5d3364247f16f531391c4a3bc3cde3bb154e1385
Zar Zar Kyaw Zin <zarzarkyawzin@gothic.local>
2020-01-16 13:22:40 +0630

    fix schedule, course, customer ui for firefox

--- src/app/components/courseplan/courseplan.component.ts
+++ src/app/components/courseplan/courseplan.component.ts
@@ -360,10 +360,7 @@ export class CourseplanComponent implements OnInit {
       name: name(),
       fees: null,
       selectedTax: { id: 1, name: 'inclusive' },
-      taxOption: [
-        { id: 1, name: 'inclusive' },
-        { id: 2, name: 'exclusive' }
-      ]
+      taxOption: [{ id: 1, name: 'inclusive' }, { id: 2, name: 'exclusive' }]
     };
     this.optArray.push(obj);
     console.log('optArray in addFeeOption', this.optArray);
@@ -375,6 +372,8 @@ export class CourseplanComponent implements OnInit {
 
   removeFeeOption(idx) {
     this.optArray.splice(idx, 1);
+    if (this.optArray.length > 1) this.checkFeeOption(1);
+    else this.isSameOpt = false;
     console.log('optArray for removeFee~~~', this.optArray);
   }
 
@@ -394,16 +393,15 @@ export class CourseplanComponent implements OnInit {
         name: key,
         fees: feeobj[key],
         selectedTax: null,
-        taxOption: [
-          { id: 1, name: 'inclusive' },
-          { id: 2, name: 'exclusive' }
-        ]
+        taxOption: [{ id: 1, name: 'inclusive' }, { id: 2, name: 'exclusive' }]
       };
       if (
         taxobj == undefined ||
         taxobj == null ||
         taxobj == '' ||
-        taxobj[key] == undefined || taxobj[key] == null || taxobj[key] == ''
+        taxobj[key] == undefined ||
+        taxobj[key] == null ||
+        taxobj[key] == ''
       ) {
         data.selectedTax = { id: 1, name: 'inclusive' };
       } else {
