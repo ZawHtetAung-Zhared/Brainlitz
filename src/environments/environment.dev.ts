6698d006f9b4f345afb0ede9b8ec9d8babf06216
Htet Shwe Yi Lin <htetshweyilin@Amdons-iMac.local>
2020-01-27 16:21:01 +0630

    update: environment.dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,5 +5,5 @@ export const environment = {
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
   // apiurl: 'http://192.168.1.106:8484'
-  apiurl: 'http://dev-app.brainlitz.com'
+  apiurl: ' https://dev-brainlitz.pagewerkz.com/'
 };
