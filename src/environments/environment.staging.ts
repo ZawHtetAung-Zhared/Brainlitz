dde73c44d2840f75c3907a51b135c9904221cf43
Htet Shwe Yi Lin <htetshweyilin@Amdons-iMac.local>
2020-01-29 14:51:55 +0630

    enableDebug: true for environment.dev

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  enableDebug: true,
+  production: true,
+  enableDebug: false,
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
