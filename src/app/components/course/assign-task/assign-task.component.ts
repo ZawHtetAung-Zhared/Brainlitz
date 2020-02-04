3d50276e0be5e82af19f36806818a7b156256e7c
Zune Zune <seinmama@amdon.com>
2020-02-04 16:52:33 +0630

    add standard and class level ui

--- src/app/components/course/assign-task/assign-task.component.ts
+++ src/app/components/course/assign-task/assign-task.component.ts
@@ -6,7 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assign-task.component.css']
 })
 export class AssignTaskComponent implements OnInit {
-  constructor() {}
+  public standActiveId: any;
+  public classActiveId: any;
+  public standardList: any = [
+    {
+      _id: '1',
+      name: 'NGSS'
+    },
+    {
+      _id: '2',
+      name: 'SingaporeMOE'
+    },
+    {
+      _id: '3',
+      name: 'Curiculum Standard I'
+    }
+  ];
+  public classList: any = [];
 
-  ngOnInit() {}
+  constructor() {}
+  ngOnInit() {
+    for (let i = 0; i < 15; i++) {
+      let temp: any = {};
+      temp._id = i;
+      temp.name = 'Primary ' + i;
+      this.classList.push(temp);
+    }
+    this.standActiveId = this.standardList[0]._id;
+  }
+
+  checkStandard(id) {
+    this.standActiveId = id;
+  }
+
+  choiceClass(id) {
+    this.classActiveId = id;
+  }
 }
