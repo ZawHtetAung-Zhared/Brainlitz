f748fb2a1638715077b94248fd53d3c04dd3bfc2
Zune Zune <seinmama@amdon.com>
2020-02-05 16:02:16 +0630

    add step 1 for schedule template

--- src/app/components/course/assign-task/assign-task.component.ts
+++ src/app/components/course/assign-task/assign-task.component.ts
@@ -9,10 +9,15 @@ declare var $: any;
 export class AssignTaskComponent implements OnInit {
   public standActiveId: any;
   public classActiveId: any;
+  public templateActiveId: any;
+  public activeStep: any;
+
+  public clickableSteps: Array<any> = ['step1'];
+  public scheduletemplateList: any = [];
+
   public isStart: boolean = false;
   public isScheduleTask: boolean = false;
-  public clickableSteps: Array<any> = ['step1'];
-  public activeStep: any;
+
   public standardList: any = [
     {
       _id: '1',
@@ -34,9 +39,18 @@ export class AssignTaskComponent implements OnInit {
     for (let i = 0; i < 50; i++) {
       let temp: any = {};
       temp._id = i;
+      temp.name = 'Template sample ' + i;
+      temp.description =
+        ' Ut sit quis proident Lorem dolore est sint ea adipisicing amet. Ex ex culpa incididunt quis nostrud sunt incididunt veniam tempor enim elit cillum dolore.';
+      this.scheduletemplateList.push(temp);
+    }
+    for (let i = 0; i < 50; i++) {
+      let temp: any = {};
+      temp._id = i;
       temp.name = 'Primary ' + i;
       this.classList.push(temp);
     }
+
     this.standActiveId = this.standardList[0]._id;
   }
 
@@ -53,6 +67,12 @@ export class AssignTaskComponent implements OnInit {
   }
 
   goToScheduleTask() {
+    $('#placeholder_color').append(
+      "<style id='feedback'>.data-name::-webkit-input-placeholder{color:" +
+        '#788796' +
+        ' !important;} .data-name::-moz-placeholder{color: #788796!important; opacity:1;} .data-name:-moz-placeholder{color: #788796 !important; opacity:1;}</style>'
+    );
+
     setTimeout(function() {
       $('#step1').addClass('active');
     }, 200);
@@ -90,5 +110,9 @@ export class AssignTaskComponent implements OnInit {
         .css('background-color', '#0080ff');
     }
     if (max != ele.parents('li').attr('id')) ele.parents('li').addClass('done');
+  }
+
+  checkTemplate(id) {
+    this.templateActiveId = id;
   }
 }
