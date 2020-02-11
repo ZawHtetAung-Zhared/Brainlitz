b2fffd124c58b04ae6af8bfc4b69ed0c10e4a6fd
Zar Zar Kyaw Zin <zarzarkyawzin@gothic.local>
2020-02-11 11:22:28 +0630

    add mastery repoports ui

---
+++ src/app/components/course/masteriesreport/sampleData.ts
@@ -0,0 +1,192 @@
+let sampleData = [
+  {
+    groupTypeValue: 'Yangon Tester Loc',
+    lessons: {
+      present: 0,
+      absent: 0,
+      notTaken: 40,
+      count: 40
+    }
+  },
+  {
+    groupTypeValue: 'Mandalay',
+    lessons: {
+      present: 5,
+      absent: 8,
+      notTaken: 3,
+      count: 16
+    }
+  },
+  {
+    groupTypeValue: 'Pyin Oo Lwin',
+    lessons: {
+      present: 1,
+      absent: 2,
+      notTaken: 0,
+      count: 3
+    }
+  },
+  {
+    groupTypeValue: 'Bagan',
+    lessons: {
+      present: 5,
+      absent: 5,
+      notTaken: 5,
+      count: 15
+    }
+  },
+  {
+    groupTypeValue: 'Kalaw',
+    lessons: {
+      present: 3,
+      absent: 3,
+      notTaken: 3,
+      count: 9
+    }
+  },
+  {
+    groupTypeValue: 'Testing Loc',
+    lessons: {
+      present: 2,
+      absent: 5,
+      notTaken: 8,
+      count: 15
+    }
+  },
+  {
+    groupTypeValue: 'Loc 1',
+    lessons: {
+      present: 0,
+      absent: 0,
+      notTaken: 3,
+      count: 10
+    }
+  },
+  {
+    groupTypeValue: 'Loc 2',
+    lessons: {
+      present: 4,
+      absent: 7,
+      notTaken: 3,
+      count: 23
+    }
+  },
+  {
+    groupTypeValue: 'Loc 3',
+    lessons: {
+      present: 5,
+      absent: 4,
+      notTaken: 3,
+      count: 20
+    }
+  },
+  {
+    groupTypeValue: 'Loc 4',
+    lessons: {
+      present: 10,
+      absent: 0,
+      notTaken: 3,
+      count: 13
+    }
+  },
+  {
+    groupTypeValue: 'Loc 5',
+    lessons: {
+      present: 0,
+      absent: 10,
+      notTaken: 3,
+      count: 13
+    }
+  },
+  {
+    groupTypeValue: 'Loc 6',
+    lessons: {
+      present: 6,
+      absent: 6,
+      notTaken: 3,
+      count: 21
+    }
+  },
+  {
+    groupTypeValue: 'Loc 7',
+    lessons: {
+      present: 8,
+      absent: 0,
+      notTaken: 3,
+      count: 17
+    }
+  },
+  {
+    groupTypeValue: 'Loc 8',
+    lessons: {
+      present: 0,
+      absent: 6,
+      notTaken: 3,
+      count: 15
+    }
+  },
+  {
+    groupTypeValue: 'Loc 9',
+    lessons: {
+      present: 0,
+      absent: 3,
+      notTaken: 0,
+      count: 3
+    }
+  },
+  {
+    groupTypeValue: 'Loc 10',
+    lessons: {
+      present: 3,
+      absent: 0,
+      notTaken: 0,
+      count: 3
+    }
+  },
+  {
+    groupTypeValue: 'Yangon Tester Loc 11',
+    lessons: {
+      present: 1,
+      absent: 5,
+      notTaken: 3,
+      count: 18
+    }
+  },
+  {
+    groupTypeValue: 'Yangon Tester Loc 12',
+    lessons: {
+      present: 3,
+      absent: 3,
+      notTaken: 20,
+      count: 30
+    }
+  },
+  {
+    groupTypeValue: 'Yangon Tester Loc 13',
+    lessons: {
+      present: 0,
+      absent: 0,
+      notTaken: 3,
+      count: 20
+    }
+  },
+  {
+    groupTypeValue: 'Yangon Tester Loc 14',
+    lessons: {
+      present: 2,
+      absent: 7,
+      notTaken: 3,
+      count: 12
+    }
+  },
+  {
+    groupTypeValue: 'Yangon Tester Loc 15',
+    lessons: {
+      present: 9,
+      absent: 4,
+      notTaken: 1,
+      count: 14
+    }
+  }
+];
+export default sampleData;
