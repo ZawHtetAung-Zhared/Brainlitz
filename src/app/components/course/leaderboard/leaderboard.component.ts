fd318394c0833d91b861db4a28eb8fbf1920d6cb
Aung Phyo Oo <aungphyooo@amdon.com>
2020-02-17 10:44:54 +0630

    Leaderboard UI

--- src/app/components/course/leaderboard/leaderboard.component.ts
+++ src/app/components/course/leaderboard/leaderboard.component.ts
@@ -8,5 +8,121 @@ import { Component, OnInit } from '@angular/core';
 export class LeaderboardComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  private _test = 1;
+
+  public showDiv: boolean = true;
+
+  public allTime = true;
+  public startDate = '';
+  public endDate = '';
+
+  test(tt: number) {
+    this._test = tt;
+  }
+
+  testing() {
+    switch (this._test) {
+      case 1:
+        this.allTime = true;
+        return 'button-parent';
+      case 2:
+        this.allTime = false;
+        return 'button-parent2';
+    }
+  }
+
+  ngOnInit() {
+    this.startDate = '1 Sep 2019';
+    this.endDate = '7 Sep 2019';
+    for (let i = 0; i < 20; i++) {
+      let first = Math.floor(Math.random() * 8);
+      let last = Math.floor(Math.random() * 8);
+      let score = Math.floor(Math.random() * 10000);
+      let updown = Math.floor(Math.random() * 20);
+      this.fakeObjGenerator(first, last, score, updown);
+    }
+
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    // this.fakeObjGenerator
+    console.log(this.leaderList);
+  }
+
+  public firstArr = [
+    'Aung',
+    'Phyo',
+    'Oo',
+    'Myo',
+    'Kyaw',
+    'Lwin',
+    'Sai',
+    'Pyae'
+  ];
+  public secondArr = [
+    'Aung',
+    'Phyo',
+    'Oo',
+    'Myo',
+    'Kyaw',
+    'Lwin',
+    'Sai',
+    'Pyae'
+  ];
+  public updownArray = [
+    -5,
+    -4,
+    -3,
+    -2,
+    -1,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    0,
+    2,
+    3,
+    -5,
+    -10,
+    -3,
+    4,
+    0,
+    10
+  ];
+
+  public leaderList = [];
+
+  fakeObjGenerator(first, last, score, updown) {
+    console.log(first, last);
+    let name = this.firstArr[first] + ' ' + this.secondArr[last];
+    let leader = {
+      id: '',
+      name: name,
+      score: score,
+      image:
+        'https://www.thersa.org/globalassets/profile-images/staff/tom-harrison-312.jpg',
+      upOrDown: this.updownArray[updown]
+    };
+    console.log(leader);
+    this.leaderList.push(leader);
+    console.log(this.leaderList);
+  }
 }
