771735d57abd181be75f7a1f7dda0ce6c4dbb3a8
Htet Shwe Yi Lin <htetshweyilin@Amdons-iMac.local>
2020-02-27 13:06:58 +0630

    change api url for environment master

--- src/environments/environment.master.ts
+++ src/environments/environment.master.ts
@@ -4,7 +4,7 @@ export const environment = {
   response_type: 'code',
   grant_type: 'authorization_code',
   orgID: '5b063e2636f2e0f83cdbac88',
-  apiurl: 'https://app.brainlitz.com',
+  apiurl: 'https://app-admin.brainlitz.com',
   courseTypeDisabled: true,
   livechatDisabled: true,
   environmentName: 'production'
