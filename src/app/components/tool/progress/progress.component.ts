39198e85bc0eca0acfac4c953b8f33859fa06f83
Htet Shwe Yi Lin <htetshweyilin@Amdons-iMac.local>
2020-03-19 16:26:56 +0630

    get all function for progress module

--- src/app/components/tool/progress/progress.component.ts
+++ src/app/components/tool/progress/progress.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewContainerRef,
+  HostListener,
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { appService } from '../../../service/app.service';
+import { ToastrService } from 'ngx-toastr';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import 'rxjs/add/operator/takeUntil';
+declare var $: any;
 
 @Component({
   selector: 'app-progress',
@@ -6,7 +19,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent implements OnInit {
-  constructor() {}
+  private regionID = localStorage.getItem('regionId');
+  private locationID = localStorage.getItem('locationId');
+  private progressModuleId;
+  private limit;
+  private skip;
 
-  ngOnInit() {}
+  constructor(
+    private _service: appService,
+    private toastr: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params);
+      this.progressModuleId = params.id;
+    });
+  }
+
+  getAllProgressAPG() {
+    this._service
+      .getAllAPG(this.regionID, this.progressModuleId, this.limit, this.skip)
+      .subscribe((res: any) => {
+        console.log('pregress apg list', res);
+      });
+  }
 }
