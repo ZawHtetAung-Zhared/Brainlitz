434a4a3eb5125010deeebc1f913fe2c659812d6d
Htet Shwe Yi Lin <htetshweyilin@Amdons-iMac.local>
2022-01-13 20:35:36 +0630

    add date picker

--- src/app/components/course/create-term/create-term.component.ts
+++ src/app/components/course/create-term/create-term.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {
   NgbModal,
   NgbDatepickerConfig,
-  NgbCalendar
+  NgbCalendar,
+  NgbDateAdapter,
+  NgbDateParserFormatter,
+  NgbDateStruct
 } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 
 @Component({
   selector: 'app-create-term',
@@ -12,14 +16,55 @@ import {
 })
 export class CreateTermComponent implements OnInit {
   @Output() closeModal = new EventEmitter<any>();
-  public modalReference: any;
 
-  constructor(private modalService: NgbModal) {}
+  public modalReference: any;
+  public hoveredDate: NgbDate | null = null;
+  public fromDate: NgbDate;
+  public toDate: NgbDate | null = null;
+
+  constructor(private modalService: NgbModal, calendar: NgbCalendar) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  }
 
   ngOnInit() {}
 
   onClickCancel() {
     console.log('onClickCancel');
     this.closeModal.emit();
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return (
+      this.fromDate &&
+      !this.toDate &&
+      this.hoveredDate &&
+      date.after(this.fromDate) &&
+      date.before(this.hoveredDate)
+    );
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return (
+      date.equals(this.fromDate) ||
+      (this.toDate && date.equals(this.toDate)) ||
+      this.isInside(date) ||
+      this.isHovered(date)
+    );
   }
 }
