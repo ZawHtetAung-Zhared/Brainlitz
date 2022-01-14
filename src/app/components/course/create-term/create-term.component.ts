12f8e3f8377226052a5ed37ed3f2ae54523385d0
Htet Shwe Yi Lin <htetshweyilin@Amdons-iMac.local>
2022-01-14 13:39:08 +0630

    updated: update date picker

--- src/app/components/course/create-term/create-term.component.ts
+++ src/app/components/course/create-term/create-term.component.ts
@@ -1,13 +1,55 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+  HostListener
+} from '@angular/core';
+import { NgModel } from '@angular/forms';
 import {
   NgbModal,
   NgbDatepickerConfig,
   NgbCalendar,
   NgbDateAdapter,
   NgbDateParserFormatter,
+  NgbInputDatepicker,
   NgbDateStruct
 } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
+import { Subscription } from 'rxjs';
+
+const now = new Date();
+const equals = (one: NgbDateStruct, two: NgbDateStruct) =>
+  one &&
+  two &&
+  two.year === one.year &&
+  two.month === one.month &&
+  two.day === one.day;
+
+const before = (one: NgbDateStruct, two: NgbDateStruct) =>
+  !one || !two
+    ? false
+    : one.year === two.year
+    ? one.month === two.month
+      ? one.day === two.day
+        ? false
+        : one.day < two.day
+      : one.month < two.month
+    : one.year < two.year;
+
+const after = (one: NgbDateStruct, two: NgbDateStruct) =>
+  !one || !two
+    ? false
+    : one.year === two.year
+    ? one.month === two.month
+      ? one.day === two.day
+        ? false
+        : one.day > two.day
+      : one.month > two.month
+    : one.year > two.year;
 
 @Component({
   selector: 'app-create-term',
@@ -18,53 +60,84 @@ export class CreateTermComponent implements OnInit {
   @Output() closeModal = new EventEmitter<any>();
 
   public modalReference: any;
-  public hoveredDate: NgbDate | null = null;
-  public fromDate: NgbDate;
-  public toDate: NgbDate | null = null;
+  public startDate: NgbDateStruct;
+  public maxDate: NgbDateStruct;
+  public minDate: NgbDateStruct;
+  public hoveredDate: NgbDateStruct;
+  public fromDate: any;
+  public toDate: any;
+  public model: any;
+  private _subscription: Subscription;
+  private _selectSubscription: Subscription;
+  @ViewChild('d') input: NgbInputDatepicker;
+  @ViewChild(NgModel) datePick: NgModel;
+  @ViewChild('myRangeInput') myRangeInput: ElementRef;
 
-  constructor(private modalService: NgbModal, calendar: NgbCalendar) {
-    this.fromDate = calendar.getToday();
-    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  isHovered = date =>
+    this.fromDate &&
+    !this.toDate &&
+    this.hoveredDate &&
+    after(date, this.fromDate) &&
+    before(date, this.hoveredDate);
+  isInside = date => after(date, this.fromDate) && before(date, this.toDate);
+  isFrom = date => equals(date, this.fromDate);
+  isTo = date => equals(date, this.toDate);
+
+  constructor(
+    private modalService: NgbModal,
+    private element: ElementRef,
+    private renderer: Renderer2,
+    private _parserFormatter: NgbDateParserFormatter
+  ) {}
+
+  ngOnInit() {
+    this.startDate = {
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      day: now.getDate()
+    };
+    this.maxDate = {
+      year: now.getFullYear() + 1,
+      month: now.getMonth() + 1,
+      day: now.getDate()
+    };
+    this.minDate = {
+      year: now.getFullYear() - 1,
+      month: now.getMonth() + 1,
+      day: now.getDate()
+    };
   }
-
-  ngOnInit() {}
 
   onClickCancel() {
     console.log('onClickCancel');
     this.closeModal.emit();
   }
 
-  onDateSelection(date: NgbDate) {
+  onDateSelection(date: NgbDateStruct) {
+    let parsed = '';
     if (!this.fromDate && !this.toDate) {
       this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+    } else if (this.fromDate && !this.toDate && after(date, this.fromDate)) {
       this.toDate = date;
+      // this.model = `${this.fromDate.year} - ${this.toDate.year}`;
+      this.input.close();
     } else {
       this.toDate = null;
       this.fromDate = date;
     }
+    if (this.fromDate) {
+      parsed += this._parserFormatter.format(this.fromDate);
+    }
+    if (this.toDate) {
+      parsed += ' - ' + this._parserFormatter.format(this.toDate);
+    }
+
+    this.renderer.setProperty(this.myRangeInput.nativeElement, 'value', parsed);
   }
 
-  isHovered(date: NgbDate) {
-    return (
-      this.fromDate &&
-      !this.toDate &&
-      this.hoveredDate &&
-      date.after(this.fromDate) &&
-      date.before(this.hoveredDate)
-    );
-  }
-
-  isInside(date: NgbDate) {
-    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return (
-      date.equals(this.fromDate) ||
-      (this.toDate && date.equals(this.toDate)) ||
-      this.isInside(date) ||
-      this.isHovered(date)
-    );
+  closeDatepicker(datePicker, event) {
+    if (event.target.offsetParent == null) datePicker.close();
+    else if (event.target.offsetParent.nodeName != 'NGB-DATEPICKER')
+      datePicker.close();
   }
 }
